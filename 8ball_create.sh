cc8e0a4b8d9d9fb29a953e34d1ad5d1333e5e5b6
Mikhail <tmv1995@gmail.com>
2019-03-22 17:45:39 +0300

    initial commit

---
+++ 8ball_create.sh
@@ -0,0 +1,53 @@
+mkdir ~/8ball
+
+cd ~/8ball
+
+sudo apt install fortune-mod
+
+echo "%
+As I see it, yes
+% 
+It is certain
+%
+It is decidedly so
+%
+Most likely
+%
+Outlook good
+%
+Signs point to yes
+%
+Without a doubt
+%
+Yes
+%
+Yes - definitely
+%
+You may rely on it
+%
+Reply hazy, try again
+%
+Ask again later
+%
+Better not tell you now
+%
+Cannot predict now
+%
+Concentrate and ask again
+%
+Don't count on it
+%
+My reply is no
+%
+My sources say no
+%
+Outlook not so good
+%
+Very doubtful
+%" >> 8ball
+
+sudo strfile 8ball
+
+echo "fortune $PWD/8ball" >> start8ball.sh
+
+sudo ln -s $PWD/start8ball.sh /usr/local/bin/8ball
